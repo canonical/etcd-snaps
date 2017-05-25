2fbc044d49a093c06b53d5316b3c3422b72d2f0e
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2017-05-25 17:23:50 -0400

    Remove cruft from test script

--- etcd-master/test_snap.sh
+++ etcd-master/test_snap.sh
@@ -6,12 +6,9 @@ snapfile="$1"
 
 snap remove etcd
 snap install $snapfile --dangerous
-snap list
-snap aliases etcd
 
 cp /snap/etcd/x1/etcd.conf.yml.sample /var/snap/etcd/common/etcd.conf.yml
 systemctl start snap.etcd.etcd.service
-hash -r
 
 ETCDCTL_API=3 /snap/bin/etcd.etcdctl --endpoints=localhost:2379 put foo bar
 ETCDCTL_API=3 /snap/bin/etcd.etcdctl --endpoints=localhost:2379 get foo
